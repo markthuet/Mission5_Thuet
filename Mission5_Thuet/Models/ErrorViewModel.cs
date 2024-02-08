3e683bcaf2db230cf5c175ae512a6d7f847b3983
markjamesthuet <markj2it@gmail.com>
2024-02-08 16:01:33 -0700

    Add project files.

---
+++ Mission5_Thuet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission5_Thuet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Mission5_Thuet/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+function calculateTotal() {
+    // Add your calculation logic here
+    var hours = parseFloat($("#hours").val());
+    var chargePerHour = 100
+
+    if (!isNaN(hours) && hours > 0) {
+        var total = hours * chargePerHour;
+        $("#total").text("Total: $" + total.toFixed(2));
+    } else {
+        alert("Please enter a valid number of hours.");
+    }
+}
